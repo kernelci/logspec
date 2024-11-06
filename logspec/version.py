3a608959129ddc3d56d0059e4bb4244a9bb8e5fa
Ricardo Cañuelo <ricardo.canuelo@collabora.com>
2024-11-06 12:49:20 +0100

    version: (trivial) fix typo
    Signed-off-by: Ricardo Cañuelo <ricardo.canuelo@collabora.com>

--- logspec/version.py
+++ logspec/version.py
@@ -7,6 +7,6 @@
 # parsing support or the output.
 # A change in the middle number means a change in the definitions of
 # states and/or transition functions.
-# A change in the middle number means a cosmetic change, refactoring or
+# A change in the minor number means a cosmetic change, refactoring or
 # any other change that doesn't impact the output or the definitions
 __version__ = '1.0.0'
